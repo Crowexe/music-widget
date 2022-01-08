c06dbed40814e568dda9c81b25867d6fa89f8b5e
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2022-01-07 19:21:17 -0600

    Falta animación de cambio y scroll lateral
    Herramienta que muestra la música que escucho en spotify o youtube music para mi stream

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import notThumbnail from './not-thumbnail.gif'
 import './App.css';
 
-function App() {
+const USER_TRACK_URL = 
+	'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=scriptedcrow&api_key=$API_KEY&format=json';
+
+const TRACK_INFO_URL = 
+	'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=$API_KEY&artist=$artistName&track=$trackName&format=json';
+
+
+const time_ms = 5000;
+
+const Widget = () => {
+	const [track, setTrack] = useState({name: '', artist: '', cover: '' });
+
+	const getCurrentSong = () => {
+		fetch(
+			USER_TRACK_URL.replace(
+				'$API_KEY',
+				process.env.REACT_APP_APIKEY
+			),
+		)
+			.then(res => res.json())
+			.then(data => {
+				const requestedTrack = {
+					name: data.recenttracks.track[0].name, 
+					artist: data.recenttracks.track[0].artist['#text'],
+					cover: '',
+				};
+				fetch(TRACK_INFO_URL.replace(
+					'$API_KEY',
+					process.env.REACT_APP_APIKEY,
+				)
+				.replace('$artistName',requestedTrack.artist)
+				.replace('$trackName',requestedTrack.name),
+				)
+				.then(res => res.json())
+				.then(data => {
+					console.log(data);
+					if(data.track.album && data.track.album.image[3]["#text"] !== "") {
+						//console.log("Cover encontrado");
+						requestedTrack.cover = data.track.album.image[3]['#text'];
+					} else {
+						//console.log("Cover no encontrado")
+						requestedTrack.cover = notThumbnail;
+					}
+					setTrack(requestedTrack);
+				});
+			});			
+	};
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			getCurrentSong();
+		}, time_ms);
+
+		return () => clearInterval(interval)
+	}, [track]);
+
+	//const changeCurrentSong = () => {
+	//	
+	//}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Widget">
+		<img 
+			className='song-cover'
+			height={300} 
+			src={track.cover} 
+			alt={`${track.artist}`}
+		/>
+		<div className='song-container'>
+			<img 
+				className='song-BG'
+				height={300} 
+				src={track.cover} 
+				alt={`${track.artist}`}
+			/>
+				<p className='title'>{track.name}</p>
+				<p className='artist'>{track.artist}</p>
+		</div> 
     </div>
   );
 }
 
-export default App;
+export default Widget;
